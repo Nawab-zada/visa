ea02b024bd567b60ce0168998e68827c39bc31fb
nawab zada <zada86829@gmail.com>
2025-02-10 19:27:54 +0500

    updated

--- src/app/components/StickyApplyButton.tsx
+++ src/app/components/StickyApplyButton.tsx
@@ -9,10 +9,8 @@ export default function StickyApplyButton() {
   return (
     <motion.button
       onClick={() => router.push("/create")}
-      initial={{ x: 120, opacity: 0 }}
-      animate={{ x: 0, opacity: 1 }}
-      transition={{ duration: 0.5 }}
-      className="fixed right-0 top-1/2 -translate-y-1/2 bg-blue-600 text-white px-6 py-3 md:px-4 md:py-2 text-lg md:text-sm rounded-l-lg flex items-center gap-2 shadow-lg hover:bg-blue-700 transition-all"
+     
+      className="fixed right-4 top-1/2  bg-blue-600 text-white px-6 py-3 md:px-4 md:py-2 text-lg md:text-sm rounded-l-lg flex items-center gap-2 shadow-lg hover:bg-blue-700 transition-all"
     >
       <span className="hidden sm:inline">Apply Now</span> 
       <FaArrowRight className="text-xl md:text-base" />
