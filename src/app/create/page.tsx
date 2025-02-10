51f017d2390bf0870be4a9825062723c31c5c343
nawab zada <zada86829@gmail.com>
2025-02-10 14:02:59 +0500

    updated

--- src/app/create/page.tsx
+++ src/app/create/page.tsx
@@ -46,7 +46,7 @@ export default function Jobs() {
     try {
       await sendEmail(job.ownerEmail, job); // Send email to job owner
       alert(`Application for ${job.title} has been sent!`);
-    } catch (error) { 
+    } catch { 
       alert("Error sending email. Please try again.");
     }
 
